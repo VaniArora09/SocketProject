aa6a1de974619f2e121dbca28b7dccd04c215b4a
vani.arora <aroravani09@gmail.com>
2018-11-05 03:08:15 -0600

    commit

--- app.js
+++ app.js
@@ -1,42 +1,27 @@
 const express=require('express');
 const app=express();
 var http  = require('http');
-var socketIO = require('socket.io');
-var fs = require('fs');
-var path = require('path');
-var server,io;
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
+let notifications=require('./push.js');
+var mysql = require('mysql');
+
+global.con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database :"tb_device_token"
+});
+
+con.connect(function(err) {
+  if (err){ throw err};
+
+  console.log("Connected!");
 });
 server = http.createServer(app);
 server.listen(8040);
-
+app.get('/push_notification', async function (req, res) {
+    let final= await notifications.sendPush();
+     res.send(final)
+   });
 app.use(express.json());
 const port = process.env.PORT || 8040;
 server.listen(port, () => console.log(`The port is listening.. ${port}`));
- io = socketIO(server);
-
- io.on('connection', function (socket) {
-//      var readStream = fs.createReadStream(path.resolve(__dirname, './test.jpeg'), {
-//          encoding: 'binary'
-//  });
-//  var chunks=[];
-//  var delay = 0;
-
-//  readStream.on('readable', function (){
-//      console.log('FILE LOADING');
-//  });
-//  readStream.on('data', function (chunk) {
-//       delay = delay +3000;
-//     setTimeout(function () {
-//      chunks.push(chunk);
-//      socket.emit('img-chunk', chunks);
-//       }, delay);
-//  });
-//  readStream.on('end', function () {
-//      console.log('FILE LOADED');
-//  });
-let readstream = fs.readFileSync(path.resolve(__dirname, "./it.jpeg"));
-    let image64 = new Buffer(readstream).toString("base64");
-    socket.emit('img-chunk', image64);
-});
